ac2f010600a5708176b57ab0273f9320d61883b1
LetMeShine <35365312+LetMeShine@users.noreply.github.com>
2025-05-19 16:41:26 +0800

    Update index.js

--- medicalServer/src/controller/index.js
+++ medicalServer/src/controller/index.js
@@ -5,12 +5,12 @@ import express from 'express'
 import UserController from './UserController.js'
 const router = express.Router();
 
-// 一定要在 router.use才生效，很多例子是写在app.use，导致一直不生效，然后一直获取不到req.body
-// 解析application/json
-router.use(express.json());
+// 一定要在 router.use才生效，很多例子是写在app.use，导致一直不生效，然后一直获取不到req.body，挪到外面去
+// // 解析application/json
+// router.use(express.json());
 
-// 解析application/x-www-form-urlencoded
-router.use(express.urlencoded({ extended: true }))
+// // 解析application/x-www-form-urlencoded
+// router.use(express.urlencoded({ extended: true }))
 
 router.use("/medical", UserController);
 
