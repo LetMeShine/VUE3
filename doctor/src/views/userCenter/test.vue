8e861e59ccd89a6d662603225702aacbdc0f8a6d
LetMeShine <35365312+LetMeShine@users.noreply.github.com>
2025-05-19 11:08:29 +0800

    Add files via upload

---
+++ doctor/src/views/userCenter/test.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        内容test++{{ key }}**{{ message }}
+        <button @click="updateLocation">{{ location }}</button>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { inject,ref } from 'vue';
+// const key = inject('key','这是默认值key')
+const message = inject('message','这是默认值')
+
+const value = inject('key', () => new ExpensiveClass(), true)
+const { location, updateLocation } = inject('location')
+</script>
+<style scoped>
+
+</style>
