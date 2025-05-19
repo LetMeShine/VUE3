563febab81d6b34864595b1bb90ecf7a028be9b1
LetMeShine <35365312+LetMeShine@users.noreply.github.com>
2025-05-19 11:05:28 +0800

    Update index.vue
    增删改查

--- doctor/src/views/userCenter/index.vue
+++ doctor/src/views/userCenter/index.vue
@@ -2,12 +2,13 @@
     <div class="user-wrap">
         <div class="condition">
             <a-button class="operate" type="primary" @click="showModal">新增 </a-button>
-            <a-input-search style="width: 300px;" v-model:value="keyword" placeholder="请输入用户名" :loading="loading"
+            <a-input-search style="width: 300px" v-model:value="keyword" placeholder="请输入用户名" :loading="loading"
                 enter-button />
         </div>
+        <Test />
         <div class="list-wrap">
             <a-table :columns="columns" :data-source="dataList">
-                <template #bodyCell="{ column, record }">
+                <template #bodyCell="{ column, record, text }">
                     <template v-if="column.key === 'tags'">
                         <span>
                             <a-tag v-for="tag in record.tags" :key="tag"
@@ -18,11 +19,13 @@
                     </template>
                     <template v-else-if="column.key === 'action'">
                         <span>
-                            <a @click="editUser">编辑</a>
+                            <a @click="editUser(record)">编辑</a>
                             <a-divider type="vertical" />
                             <a>重置密码</a>
                             <a-divider type="vertical" />
-                            <a>删除</a>
+                            <a-popconfirm title="确定要删除该用户？" @confirm="deleteUser(record.id)">
+                                <a>删除</a>
+                            </a-popconfirm>
                         </span>
                     </template>
                 </template>
@@ -55,7 +58,7 @@
                 </a-form-item>
             </a-form>
             <template #footer>
-                <a-button @click="hideModal">取消</a-button>
+                <a-button @click="open = false">取消</a-button>
                 <a-button type="primary" @click="hideModal">确定</a-button>
             </template>
         </a-modal>
@@ -63,85 +66,120 @@
 </template>
 
 <script lang="ts" setup>
-import { ref, computed, onBeforeMount, reactive } from 'vue';
-import { columns } from './ts/columns'
-import type { TableProps } from 'ant-design-vue/lib/table/Table'
+    import { ref, computed, onBeforeMount, reactive, provide } from 'vue'
+    import { columns } from './ts/columns'
+    import type { TableProps } from 'ant-design-vue/lib/table/Table'
 
-import { getUserList, addUser } from '@/API/index.ts'
+    import { getUserList, addUser, updateUser, deletUser } from '@/API/index.ts'
+    import Test from './test.vue'
 
-interface FormState {
-    username?: string | undefined;
-    password?: string | undefined;
-    name?: string | undefined;
-    birth?: string | undefined;
-    address?: string | undefined;
-    tel?: string | undefined;
-}
+    provide('key', 666)
+    const location = ref('North Pole')
 
-const keyword = ref<string>('');
-const loading = ref<Boolean>(false);
-const open = ref<boolean>(false);
-const isAdd = ref<boolean>(false);
-const title = computed(() => {
-    return isAdd.value ? '新增' : '编辑'
-})
-
-const dataList = ref<TableProps["dataSource"]>([]);
-
-const showModal = () => {
-    isAdd.value = true;
-    open.value = true;
-};
-const hideModal = () => {
-    open.value = false;
-    let param = {
-        userCode: Math.random() + '',
-        name: formState.name,
-        username: formState.username,
-        password: formState.password,
-        address: formState.address,
-        tel: formState.tel,
-        // birth
+    function updateLocation() {
+        location.value = 'South Pole'
     }
-    addUser(param).then(() => {
+
+    provide('location', {
+        location,
+        updateLocation
+    })
+
+    interface FormState {
+        username?: string | undefined
+        password?: string | undefined
+        name?: string | undefined
+        birth?: string | undefined
+        address?: string | undefined
+        tel?: string | undefined
+    }
+
+    const keyword = ref<string>('')
+    const loading = ref<Boolean>(false)
+    const open = ref<boolean>(false)
+    const isAdd = ref<boolean>(false)
+    const title = computed(() => {
+        return isAdd.value ? '新增' : '编辑'
+    })
+
+    const dataList = ref<TableProps['dataSource']>([])
+
+    const showModal = () => {
+        isAdd.value = true
+        open.value = true
+    }
+    const hideModal = () => {
+        let param = {
+            name: formState.name,
+            username: formState.username,
+            password: formState.password,
+            address: formState.address,
+            tel: formState.tel
+            // birth
+        }
+        if (isAdd.value) {
+            param.userCode = Math.random() + ''
+            addUser(param).then(() => {
+                open.value = false
+                getUserList().then(({ data }) => {
+                    console.log(data, 55555)
+                    dataList.value = data.data
+                })
+            })
+        } else {
+            // 更新
+            updateUser(formState).then(() => {
+                open.value = false
+                getUserList().then(({ data }) => {
+                    console.log(data, 55555)
+                    dataList.value = data.data
+                })
+            })
+        }
+    }
+
+    const deleteUser = (id) => {
+        console.log(id, 1555)
+        deletUser(id).then(() => {
+            getUserList().then(({ data }) => {
+                console.log(data, 55555)
+                dataList.value = data.data
+            })
+        })
+    }
+
+    const editUser = (record) => {
+        console.log(record, 1555)
+        formState = record
+        isAdd.value = false
+        open.value = true
+    }
+
+    let formState = reactive({} as FormState)
+    const onFinish = (values: any) => {
+        console.log('Success:', values)
+    }
+
+    const onFinishFailed = (errorInfo: any) => {
+        console.log('Failed:', errorInfo)
+    }
+
+    onBeforeMount(() => {
         getUserList().then(({ data }) => {
             console.log(data, 55555)
             dataList.value = data.data
         })
     })
-};
-
-const editUser = () => {
-    isAdd.value = false;
-    open.value = true;
-}
-
-const formState = reactive({} as FormState);
-const onFinish = (values: any) => {
-    console.log('Success:', values);
-};
-
-const onFinishFailed = (errorInfo: any) => {
-    console.log('Failed:', errorInfo);
-};
-
-onBeforeMount(() => {
-    getUserList().then(({ data }) => {
-        console.log(data, 55555)
-        dataList.value = data.data
-    })
-})
-
 </script>
 <style scoped>
-.user-wrap {
-    width: 100%;
-    height: 100%;
-}
+    .user-wrap {
+        width: 100%;
+        height: 100%;
+    }
 
-.condition {
-    margin-bottom: 16px;
-    display: flex;
-    justify-content: space-between;
-}
+    .condition {
+        margin-bottom: 16px;
+        display: flex;
+        justify-content: space-between;
+    }
 </style>
