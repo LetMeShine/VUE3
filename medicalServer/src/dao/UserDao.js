47feea1472980f552a6d0c49e048cf8d494653d7
LetMeShine <35365312+LetMeShine@users.noreply.github.com>
2025-05-19 11:07:12 +0800

    Update UserDao.js

--- medicalServer/src/dao/UserDao.js
+++ medicalServer/src/dao/UserDao.js
@@ -18,6 +18,28 @@ class UserDao {
         const userInfo = await db.Users.create(param)
         return userInfo;
     }
+    async updateUser(param) {
+        console.log("==========update", param)
+        let id = param.id
+        let updateAttra = ['name', 'password', 'address', 'tel']
+        let updateParam = {}
+        updateAttra.forEach(attr => {
+            updateParam[attr] = param[attr]
+        })
+        const userInfo = await db.Users.update(
+            updateParam, // 要更新的字段
+            { where: { id,  } } // 匹配的条件
+          )
+        return userInfo;
+    }
+    async deleteUser(id) {
+        console.log("==========delete", id)
+        // 或者直接使用 delete 也可以删除
+        const userInfo = await db.Users.destroy(
+            { where: { id } } // 匹配的条件
+          )
+        return userInfo;
+    }
 }
 const userDaoInstance = new UserDao()
 export default userDaoInstance
